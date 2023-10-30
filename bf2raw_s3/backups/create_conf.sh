ad2176db699822186b7138431b080e6a7f15a065
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-10-30 21:53:06 +0100

    Delete bf2raw_s3/backups directory

--- bf2raw_s3/backups/create_conf.sh
+++
@@ -1,9 +0,0 @@
-rclone config create minio s3 \
-provider=Minio \
-access_key_id=eosc-future-user \
-secret_access_key=w2xx9EatWwmtsrbewt3LEfiGB \
-endpoint=s3.embl.de \
-acl=private > .rclone.conf
-
-
-
--- bf2raw_s3/backups/mount.sh
+++
@@ -1,3 +0,0 @@
-#rclone --config /home/oezdemir/PycharmProjects/rclone/mount_s3/.rclone.conf mount minio:eosc-future /home/oezdemir/PycharmProjects/rclone/s3mount
-rclone --config $__tool_directory__/.rclone.conf mount minio:$1 $2
-
